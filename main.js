479c2db8350974a8018e07a328c0aa839e92e062
Thomas Merkel <tm@core.io>
2012-05-21 07:17:22 +0000

    first functions: /list, /list/:id

--- main.js
+++ main.js
@@ -16,9 +16,30 @@ var
 conf.port = conf.port || 8080;
 conf.uri = conf.uri || 'qemu:///system';
 
+// libVirt connection
+var hypervisor = new Hypervisor(conf.uri);
+var domain;
+
 // server
-app.get('/', function(req, res){
-	res.send('hello world');
+app.get('/list', function(req, res) {
+	names = new Array();
+	for (ad in hypervisor.getActiveDomains()) {
+		name = hypervisor.lookupDomainById(hypervisor.getActiveDomains()[ad]).getName();
+		names[names.length] = { id : hypervisor.getActiveDomains()[ad], name : name, online: true };
+	}
+	for (dd in hypervisor.getDefinedDomains()) {
+		name = hypervisor.lookupDomainById(hypervisor.getDefinedDomains()[dd]).getName();
+		names[names.length] = { id : hypervisor.getActiveDomains()[dd], name : name, online: false };
+	}
+	res.send(names);
+});
+app.get('/list/:id', function(req, res) {
+	domain = hypervisor.lookupDomainById(parseInt(req.params.id));
+	domainInfo = domain.getInfo();
+	domainInfo.name = domain.getName();
+	
+	if (domainInfo != null)
+		res.send(domainInfo);
 });
 
 app.listen(conf.port);
